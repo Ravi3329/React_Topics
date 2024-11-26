5254870196f41444da33b4bba9fae7ee6bb3adf3
Ravi3329 <ravichalla4456@gmail.com>
2024-11-26 08:29:23 +0530

    Counter_Project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,13 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(5)
+
+  function Addvalue(){
+    setCount(count+1)
+    console.log(count);
+  }
+
+  function Removevalue(){
+    setCount(count-1)
+    console.log(count)
+  }
 
   return (
     <>
+
       <div>
         <h1>React Counter</h1>
-        
+        <h2>Counter Value:{count}</h2>
+        <button onClick={Addvalue}>Add value</button>
+        {"     "}
+        <button onClick={Removevalue}>Remove Value</button>
+        <p>Footer:{count}</p>
       </div>
     </>
   )
